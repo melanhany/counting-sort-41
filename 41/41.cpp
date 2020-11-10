08374641bd5c7dcdb177a7ec915406900ba89d35
melanhany <57508121+melanhany@users.noreply.github.com>
2020-11-10 22:17:09 +0500

    Add files via upload

---
+++ 41/41.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <cmath>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	ifstream fin("INPUT.TXT");
+	ofstream fout("OUTPUT.TXT");
+	int n, i;
+	int massiv_n[201];
+	for (i = 0; i < 201; i++)
+		massiv_n[i] = 0;
+
+	fin >> n; //кол-во участков
+	for (i = 0; i < n; i++) {
+		int temp;
+		fin >> temp;
+		massiv_n[temp + 100]++;
+	}
+	
+
+	for (i = 0; i < 201; i++)
+		for (int j = 0; j < massiv_n[i]; j++)
+			fout << i - 100 << ' ';
+		/*if (massiv_n[i] != 0) {
+			fout << i - 100 << ' ';
+		}*/
+	return 0;
+}
